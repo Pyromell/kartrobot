3bfbab716342fe7aad6ab12ebd0670500b55e731
Christoffer Lundell <chrlu470@student.liu.se>
2024-11-07 15:04:50 +0100

    fix uart

--- kommunikationsmodul/backend/main.py
+++ kommunikationsmodul/backend/main.py
@@ -26,37 +26,39 @@ client_socket.listen(9)
 
 def uart_send(data: bytes):
     try:
-        sensor_ttyUSB.write(data.encode(encoding='utf-8'))
-        read_buf = sensor_ttyUSB.read(256)
-        if read_buf:
-            print("Read from sensor module: " +
-                  read_buf.decode(encoding='utf-8'))
-        print("read " + str(len(read_buf)) + " amount of bytes")
+        sensor_ttyUSB.write(data)
     except serial.SerialException as e:
         print(f"serial error: {e}")
 
     if 'sensor_ttyUSB' in locals() and sensor_ttyUSB.is_open:
         serial_port.close()
 
+def uart_recv() -> bytes:
+   read_buf = sensor_ttyUSB.read(256)
+   if read_buf:
+       print("Read from sensor module: " + read_buf.decode('utf-8'))
+   print("read " + str(len(read_buf)) + " amount of bytes")
+
 
 def main() -> int:
     try:
         conn, address = client_socket.accept()
+        print('Got new connection')
         conn.setblocking(0)
         while True:
             ready = select.select([conn], [], [], 1.0)
-            print('Got new connection')
             if ready[0]:
                 data = conn.recv(3)
                 # print("Data: " data.decode(encoding='utf-8')
-                print("Got data")
+                # print("Got data")
                 if len(data) == 0:
                     print('Disconnected')
                     break
                 # TODO: Decide what to do with incoming data
                 match int.from_bytes(data, 'big'):
                     case 0:
-                        uart_send(b'Got message start/stop')
+                        print("Sending Hello, world!")
+                        uart_send(b'Hello, world!')
                     case 1:
                         uart_send(b'Got message fram')
                     case 2:
@@ -66,6 +68,7 @@ def main() -> int:
                     case 4:
                         uart_send(b'Got message manuell / autonom')
             # TODO: Send sensor and map data
+            uart_recv()
     except KeyboardInterrupt:
         return 1
 
