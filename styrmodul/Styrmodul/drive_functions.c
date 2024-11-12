fd4cdfc46199bcffdf03fd64a3608e56a2695cfb
Johan Lindberg <johli245@student.liu.se>
2024-11-12 12:34:04 +0100

    Basic UART steering is implemented.

--- styrmodul/Styrmodul/drive_functions.c
+++ styrmodul/Styrmodul/drive_functions.c
@@ -1,30 +1,22 @@
-uint8_t speed_div_1 (void) {
-	return TCNT0;
-}
+uint8_t speed_div_1 (void) { return TCNT0; }
 
-uint8_t speed_div_8 (void) {
-	return TCNT1;
-}
+uint8_t speed_div_8 (void) { return TCNT1; }
 
-uint8_t speed_div_64 (void) {
-	return TCNT2;
-}
+uint8_t speed_div_64 (void) { return TCNT2; }
 
-uint8_t speed_div_256 (void) {
-	return TCNT3;
-}
+uint8_t speed_div_256 (void) { return TCNT3; }
 
-uint8_t speed_select(const uint8_t speed) {
-	switch (speed) {
-		case 0:
+uint8_t speed_select(const uint8_t* speed) {
+	switch (*speed) {
+		case 0x00:
 			return 0x00;
-		case 1:
+		case 0x01:
 			return TCNT0; // 50% on, 50 % off
-		case 2:
+		case 0x02:
 			return TCNT0 | TCNT1; // 75% on, 25 % off
-		case 3:
+		case 0x03:
 			return TCNT0 | TCNT1 | TCNT2; // 87,5% on, 12,5% off
-		case 4:
+		case 0x04:
 			return TCNT0 | TCNT1 | TCNT2 | TCNT3; // 93,75% on, 6,25% off
 		default:
 			return TCNT0; // 50% on, 50 % off
@@ -32,25 +24,24 @@ uint8_t speed_select(const uint8_t speed) {
 	return TCNT0; //FAILSAFE
 }
 
-
-void north(const uint8_t speed_left, const uint8_t speed_right)
+void north(const uint8_t* speed_left, const uint8_t* speed_right)
 {
-	PORTD = ( (speed_select(speed_left) & 0x80) | ((speed_select(speed_right) >> 1) & 0x40) ) | 0x30;
+	PORTD = ( (speed_select(*speed_left) & 0x80) | ((speed_select(*speed_right) >> 1) & 0x40) ) | 0x30;
 }
 
-void south(const uint8_t speed_left, const uint8_t speed_right)
+void south(const uint8_t* speed_left, const uint8_t* speed_right)
 {
-	PORTD = ( (speed_select(speed_left) & 0x80) | ((speed_select(speed_right) >> 1) & 0x40) ) | 0x00;
+	PORTD = ( (speed_select(*speed_left) & 0x80) | ((speed_select(*speed_right) >> 1) & 0x40) ) | 0x00;
 }
 
-void west(const uint8_t speed_left, const uint8_t speed_right)
+void west(const uint8_t* speed_left, const uint8_t* speed_right)
 {
-	PORTD = ( (speed_select(speed_left) & 0x80) | ((speed_select(speed_right) >> 1) & 0x40) ) | 0x20;
+	PORTD = ( (speed_select(*speed_left) & 0x80) | ((speed_select(*speed_right) >> 1) & 0x40) ) | 0x20;
 }
 
-void east(const uint8_t speed_left, const uint8_t speed_right)
+void east(const uint8_t* speed_left, const uint8_t* speed_right)
 {
-	PORTD = ( (speed_select(speed_left) & 0x80) | ((speed_select(speed_right) >> 1) & 0x40) ) | 0x10;
+	PORTD = ( (speed_select(*speed_left) & 0x80) | ((speed_select(*speed_right) >> 1) & 0x40) ) | 0x10;
 }
 
 void stop()
@@ -58,8 +49,35 @@ void stop()
 	PORTD = 0x00;
 }
 
-void drive()
+/***********************************
+	Drive Instruction Manual:
+	N: Drive forward
+	S: Reverse
+	W: Turn left
+	E: Turn right
+	
+	X: Stop
+***********************************/
+
+void drive(const uint8_t* drive_instr)
 {
+  cli();
+  switch(*drive_instr)
+  {
+    case 'N': north(1,1); break;
+    case 'S': south(1,1); break;
+    case 'W': west(1,1); break;
+    case 'E': east(1,1); break;
+    case 'X': stop(); break;
+    default: stop(); break; //FAILSAFE
+  }
+  UART_Transmit_Instr_Done();
+  sei();
+}
+
+void drive_test()
+{
+	cli();
 	for (int j = 0; j < 30; ++j)
 		for (int i = 0; i < 9999; ++i)
 			north(1,1);
@@ -75,7 +93,7 @@ void drive()
 	for (int j = 0; j < 99; ++j)
 		for (int i = 0; i < 9999; ++i)
 			stop();
-	
+	sei();
 }
 
 void drive_40_cm(const uint8_t speed_left, const uint8_t speed_right)
