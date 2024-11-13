bfc883a6a84f0113a79901c70bbee9c1bafe9f90
Christoffer Lundell <chrlu470@student.liu.se>
2024-11-13 12:07:36 +0100

    progress 13/11

--- kommunikationsmodul/interface/main.py
+++ kommunikationsmodul/interface/main.py
@@ -2,21 +2,47 @@ import tkinter
 import socket
 import time
 import pickle
+import select
+from enum import Enum
+from struct import unpack
 
 from tkinter import *
 
 pi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 pi_socket.connect(("10.42.0.1", 8027))
+pi_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+pi_socket.setblocking(0)
+
+
+class SquareState(Enum):
+    UNKNOWN = 0
+    EMPTY = 1
+    WALL = 2
 
 
 class Interface():
+    buffer = b''
 
     def recieve(self):
-        read_buf = pi_socket.recv(1028)
-        print(read_buf)
-        # <++>
-        print(pickle.loads(read_buf))
-        #self.tk.after(1000, self.recieve)
+        ready = select.select([pi_socket], [], [], 0.2)
+        if ready[0]:
+            self.buffer += pi_socket.recv(11688)
+            print(self.buffer)
+            try:
+                messageData = pickle.loads(self.buffer)
+                self.sensorTextBox.insert(
+                    tkinter.END,
+                    chars=str(messageData['sensors']),
+                )
+            except pickle.UnpicklingError:
+                print("Incomplete message...")
+                self.tk.after(1000, self.recieve)
+                return
+            self.buffer = self.buffer[11688:]
+        else:
+            print("No data...")
+        self.tk.after(1000, self.recieve)
+
     def __init__(self):
         self.tk = Tk()
         self.buttonStartStop = Button(
@@ -65,30 +91,33 @@ class Interface():
         self.tk.mainloop()
 
 
-
-
-
 def sendStartStop():
+    print("Sending Start / Stop")
     pi_socket.send((0).to_bytes(3, 'big'))
 
 
 def sendForward():
+    print("Sending Forward")
     pi_socket.send((1).to_bytes(3, 'big'))
 
 
 def sendBack():
+    print("Sending Back")
     pi_socket.send((2).to_bytes(3, 'big'))
 
 
 def sendRight():
+    print("Sending Right")
     pi_socket.send((3).to_bytes(3, 'big'))
 
 
 def sendLeft():
+    print("Sending Left")
     pi_socket.send((4).to_bytes(3, 'big'))
 
 
 def sendManualToggle():
+    print("Sending Manual Toggle")
     pi_socket.send((5).to_bytes(3, 'big'))
 
 
